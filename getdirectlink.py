963babec119f163eb0f9f0db321926e0e8fd9c2b
atony <xujiayang12@outlook.com>
2019-05-11 14:15:34 +0800

    v1

---
+++ getdirectlink.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+import json
+import csv
+
+
+def change(ori_link):
+    str_part = ori_link.split('/')
+    args = str_part[7].split('?')
+    out = 'https://' + str_part[2] + '/personal/' + str_part[6] + '/_layouts/15/download.aspx?' + args[1] + '&share=' + \
+          args[0]
+    return out
+
+
+def generate_json(data, cover, artist):
+    all_data = []
+    for i in data:
+        one = {}
+        one['name'] = i[1]
+        one['url'] = change(i[0])
+        one['cover'] = change(cover)
+        one['artist'] = artist
+        all_data.append(one)
+    json_data = json.dumps(all_data)
+    print(json_data)
+
+
+if __name__ == '__main__':
+    filename = 'music.csv'
+    with open(filename) as f:
+        reader = csv.reader(f)
+        data = list(reader)
+        generate_json(data,'https://vlityacid-my.sharepoint.com/:i:/g/personal/cc07_vlity_com/EcUxuQxXxIxPjN613tp1WCgBHVG_dpcMWtFjkEVkyaCtKA?e=P53IJg','星之海')
---
+++ static/album.js
@@ -0,0 +1,108 @@
+var ap = new APlayer({
+    element: document.getElementById('aplayer'),
+    narrow: false,
+    autoplay: false,
+    showlrc: false,
+    listFolded: false,
+    listMaxHeight: 100,
+    audio:
+        [{
+            "name": "8102",
+            "url": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=p5jI0O&share=Eef0dyyR5GdDkdJAfsM-zC4Bwe52Qh2uyXABKWipBCO3aw",
+            "cover": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=De5Xlm&share=ESYwbzMAg59OknkHTomY-1IBquvlyOZK-xqWtWGDatH64g",
+            "artist": "Lost in Tianyi"
+        }, {
+            "name": "\u54d2\u5566\u5566",
+            "url": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=0DbXRF&share=Eask3zmQdQpPoJ99brNkUoMB2Rpb7yFj2Ifswb_6wYwY0g",
+            "cover": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=De5Xlm&share=ESYwbzMAg59OknkHTomY-1IBquvlyOZK-xqWtWGDatH64g",
+            "artist": "Lost in Tianyi"
+        }, {
+            "name": "\u6700\u5927\u5316+",
+            "url": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=4bAsQe&share=EVAz6r8EyhtKm6e03jM9SJoBzhgukdajLODozblYOPalyw",
+            "cover": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=De5Xlm&share=ESYwbzMAg59OknkHTomY-1IBquvlyOZK-xqWtWGDatH64g",
+            "artist": "Lost in Tianyi"
+        }, {
+            "name": "\u51e4\u516e\u51e4\u516e",
+            "url": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=i70bQ8&share=EedzRXo694pIiqlmmho9UtYB9aSUoznSdKO_Utc-YOSR1w",
+            "cover": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=De5Xlm&share=ESYwbzMAg59OknkHTomY-1IBquvlyOZK-xqWtWGDatH64g",
+            "artist": "Lost in Tianyi"
+        }, {
+            "name": "\u65af\u82ac\u514b\u65af\u7684\u8c1c\u5e95",
+            "url": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=uAiyBb&share=ET2gHedzNtFLiFcAAjUm8S0BeDr-K1Ubp5SzpJgJMFcamA",
+            "cover": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=De5Xlm&share=ESYwbzMAg59OknkHTomY-1IBquvlyOZK-xqWtWGDatH64g",
+            "artist": "Lost in Tianyi"
+        }, {
+            "name": "\u7b14\u8bb0\u672c\u4e0a\u7684\u5e7b\u60f3\u4e16\u754c",
+            "url": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=OhsAAX&share=ERVm_HmqCcNCu5httaIjI3oBlHmXNc6yYL5KXslN_du3Hg",
+            "cover": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=De5Xlm&share=ESYwbzMAg59OknkHTomY-1IBquvlyOZK-xqWtWGDatH64g",
+            "artist": "Lost in Tianyi"
+        }, {
+            "name": "\u65af\u4eba",
+            "url": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=Jq865M&share=EVj0UWfV5_pAvokG38a_P2YBMuetWePFy16d2DYOiAJ5WQ",
+            "cover": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=De5Xlm&share=ESYwbzMAg59OknkHTomY-1IBquvlyOZK-xqWtWGDatH64g",
+            "artist": "Lost in Tianyi"
+        }, {
+            "name": "\u5618\u3064\u304d\u306f\u604b\u306e\u306f\u3058\u307e\u308a",
+            "url": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=0WXMYd&share=EfADVUGQfyVGlwa582t-YSsBzxFonD3JCOT7NBXY37nJZg",
+            "cover": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=De5Xlm&share=ESYwbzMAg59OknkHTomY-1IBquvlyOZK-xqWtWGDatH64g",
+            "artist": "Lost in Tianyi"
+        }, {
+            "name": "Monologue",
+            "url": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=SoFZMf&share=ERjKcgknLiNCgQL0UztHBQcBiOpPwx68bMMMkHExMlhLhw",
+            "cover": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=De5Xlm&share=ESYwbzMAg59OknkHTomY-1IBquvlyOZK-xqWtWGDatH64g",
+            "artist": "Lost in Tianyi"
+        },
+            {
+                "name": "Awakening",
+                "url": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=OZnin1&share=EZNuR5vdJ4BNjv4NIXO3X8wBhMly76uOhsero2FlAn8bkg",
+                "cover": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=P53IJg&share=EcUxuQxXxIxPjN613tp1WCgBHVG_dpcMWtFjkEVkyaCtKA",
+                "artist": "\u661f\u4e4b\u6d77"
+            }, {
+            "name": "STOP",
+            "url": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=b1PhMW&share=EdDW8p_8_INHrlJQ84mTJKMBaSGZB4RqxjibKEkXYSmDFg",
+            "cover": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=P53IJg&share=EcUxuQxXxIxPjN613tp1WCgBHVG_dpcMWtFjkEVkyaCtKA",
+            "artist": "\u661f\u4e4b\u6d77"
+        }, {
+            "name": "\u7f51\u4e00\u5c3e\u597d\u68a6",
+            "url": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=UGM3Nm&share=EQl88kuqTuZJufeWgXLjDMEBgqQk1PcZI1xA7vH9aGUwwA",
+            "cover": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=P53IJg&share=EcUxuQxXxIxPjN613tp1WCgBHVG_dpcMWtFjkEVkyaCtKA",
+            "artist": "\u661f\u4e4b\u6d77"
+        }, {
+            "name": "\u6d9f\u6f2a \u661f\u6d77Ver.",
+            "url": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=cXygGx&share=EVi67u162w9FtWp_r4YHi4kBywTV8dHK5iC6WgJGDpN9cw",
+            "cover": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=P53IJg&share=EcUxuQxXxIxPjN613tp1WCgBHVG_dpcMWtFjkEVkyaCtKA",
+            "artist": "\u661f\u4e4b\u6d77"
+        }, {
+            "name": "\u6d45\u6ee9",
+            "url": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=yuPEsE&share=EYzmVzMN1bdMpAd_AUsxlYEBbhGvQpkY5aKj9nBMVSGwxQ",
+            "cover": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=P53IJg&share=EcUxuQxXxIxPjN613tp1WCgBHVG_dpcMWtFjkEVkyaCtKA",
+            "artist": "\u661f\u4e4b\u6d77"
+        }, {
+            "name": "\u8bb0\u5fc6\u6b8b\u7247",
+            "url": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=7r24WB&share=EbZafdt3OOtDn5P10ObnokABPS-Lorsahee3le4CMsWGgw",
+            "cover": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=P53IJg&share=EcUxuQxXxIxPjN613tp1WCgBHVG_dpcMWtFjkEVkyaCtKA",
+            "artist": "\u661f\u4e4b\u6d77"
+        }, {
+            "name": "\u62c2\u6653",
+            "url": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=whGjNR&share=EVj8Uq9RwP1Lo_PoOwPSqX4BHAqADpqTN_pylNGhQV7feA",
+            "cover": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=P53IJg&share=EcUxuQxXxIxPjN613tp1WCgBHVG_dpcMWtFjkEVkyaCtKA",
+            "artist": "\u661f\u4e4b\u6d77"
+        }, {
+            "name": "\u7eee\u4e91\u6cfd",
+            "url": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=0XIuIk&share=EUHDp91DZixNsjH36-RQ7DUBkaQxeuIIseP6XGJmoRu5Qg",
+            "cover": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=P53IJg&share=EcUxuQxXxIxPjN613tp1WCgBHVG_dpcMWtFjkEVkyaCtKA",
+            "artist": "\u661f\u4e4b\u6d77"
+        }, {
+            "name": "Jounery",
+            "url": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=Rs0Knl&share=ES_D_uQSc8hOsJsH7clQbwwBHyDT1sxwt9FWFIAE5phG1A",
+            "cover": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=P53IJg&share=EcUxuQxXxIxPjN613tp1WCgBHVG_dpcMWtFjkEVkyaCtKA",
+            "artist": "\u661f\u4e4b\u6d77"
+        }, {
+            "name": "\u6d45\u6ee9 \u661f\u5c18&\u6d77\u4f0a",
+            "url": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=uRpbs5&share=ERMrRJ5uBRhElIJqQZepr8UBi9p6Prev90lFFUC5BS1y5Q",
+            "cover": "https://vlityacid-my.sharepoint.com/personal/cc07_vlity_com/_layouts/15/download.aspx?e=P53IJg&share=EcUxuQxXxIxPjN613tp1WCgBHVG_dpcMWtFjkEVkyaCtKA",
+            "artist": "\u661f\u4e4b\u6d77"
+        }]
+});
+ap.init();
+
